f0c000d815f9b4e9280cd9b0e6e9b56d0f39885f
pravin <pravin.deshmukh12@gmail.com>
2025-06-27 10:45:17 +0530

    path

--- diet-tracker-react/server.js
+++ diet-tracker-react/server.js
@@ -459,11 +459,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve static files from the React build output in the monorepo
-app.use(express.static(path.join(__dirname, 'diet-tracker-react', 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // SPA fallback (must be last route)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'diet-tracker-react', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 connectToMongoDB().then(() => {
